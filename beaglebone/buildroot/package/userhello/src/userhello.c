43581366e8622c21983c9912b99c20280c087484
hamidrahmani <hamid.rahmani@bbraun.com>
2025-07-24 20:39:00 +0200

    synch the buildroot code

--- beaglebone/buildroot/package/userhello/src/userhello.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("userHello is a user package just for testing that building the custom package under current system setup is working! \n");
-    return 0;
-}
